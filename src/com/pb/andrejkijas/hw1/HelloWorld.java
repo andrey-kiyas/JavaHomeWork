a3422d20236cddb89a516576f5f35e0f96dcf985
andrey-kiyas <andrey.kiyas@gmail.com>
2021-10-13 17:01:11 +0300

    init commit

--- src/com/pb/andrejkijas/hw1/HelloWorld.java
+++ src/com/pb/andrejkijas/hw1/HelloWorld.java
@@ -1,5 +1,10 @@
 package com.pb.andrejkijas.hw1;
 
+/*
+- Воспроизвести труктуру проекта
+- Разместить на сайте GitHub (https://github.com/) и отправить на проверкт в
+  Google Classroom (https://classroom.google.com/c/NDAwNjE5OTMwNjc3/a/NDA3MTExMDQ4Mzk4/details) ссылку на проект
+*/
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
