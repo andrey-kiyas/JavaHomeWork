8f24f68ffb51ad0ac66ddf7e5deaea9ab0106889
andrey-kiyas <andrey.kiyas@gmail.com>
2021-11-20 17:32:01 +0200

    init commit

---
+++ src/com/pb/andrejkijas/hw9/FileNumbers.java
@@ -0,0 +1,65 @@
+package com.pb.andrejkijas.hw9;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Random;
+
+/**
+ * В пакете hw9 создать класс FileNumbers.
+ * В нем создать методы:
+ *
+ * - createNumbersFile - создает текстовый файл "numbers.txt" заполненный случайными целыми числами от 1 до 99.
+ * 10 строк и по 10 чисел в каждой строке. Числа разделять пробелами.
+ *
+ * - createOddNumbersFile - читает файл "numbers.txt", и создает на основе него новый файл "odd-numbers.txt" в который
+ * входят все числа из "numbers.txt" только все четные заменены на 0.
+ *
+ * В методе main продемонстрировать работу методов createNumbersFile и createOddNumbersFile.
+ *
+ * По желанию можно добавить логирование работы программы.
+ * **/
+
+public class FileNumbers {
+    public static void main(String[] args) throws Exception{
+        createNumbersFile();
+        createOddNumbersFile();
+    }
+
+    private static void createNumbersFile(){
+        int[] arrayRW = new int[100];
+        Random random = new Random();
+        for (int i = 0; i < arrayRW.length; i++) {
+            arrayRW[i] = 0 + random.nextInt(99 - 0 + 1);
+        }
+        try (Writer writer = new FileWriter("files09/numbers.txt")) {
+            for (int j = 0; j <= arrayRW.length+1; j++) {
+                if ((j) % 10 == 0) {
+                    if(j == 0){
+                        writer.write(arrayRW[j] + " ");
+                    } else {
+                        writer.write(arrayRW[j] + " \n");
+                    }
+                } else {
+                    writer.write(arrayRW[j] + " ");
+                }
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        System.out.println("Данные созданы и записаны в файл \"numbers.txt\"");
+    }
+
+    private static void createOddNumbersFile(){
+        char[] arrayR = new char[1000];
+        try (Reader reader = new FileReader("files09/numbers.txt")) {
+            reader.read(arrayR);
+            System.out.println("\nДанные файла");
+            System.out.println(arrayR);
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("Файл не найден");
+        }
+    }
+}
